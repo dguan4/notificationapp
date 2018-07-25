364ef504578ae23ade5dd5021579ed4942213211
dguan4 <dguan4@illinois.edu>
2018-07-24 20:08:09 -0500

    Making this main page

--- pages/index.vue
+++ pages/index.vue
@@ -1,73 +1,35 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        nuxtproject
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <input v-model="url" type="text" name="url" />
-      <button v-on:click="runScript()">Run Script</button>
-    </div>
-  </section>
+  <div>
+      <h1>hello world</h1>
+      <input v-model="msg" type="text">
+      <button v-on:click="sendMessage()" >Send SMS</button>
+      <h2 v-text="msg"></h2>
+  </div>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
 import axios from 'axios'
 
 export default {
   data () {
-    return {
-      url: ''
-    }
-  },
-  components: {
-    AppLogo
+      return {
+          msg: ''
+      }
   },
   methods: {
-    runScript() {
-      axios.get(this.url)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((err) => {
-          console.err(err);
-        })
-    }
+      sendMessage() {
+          console.log(this.msg)
+          axios.post('/api/sendmsg', { 'msg': this.msg })
+            .then(res => {
+                console.log("this works", res)
+            })
+            .catch(err => console.error(err))
+      }
   }
 }
 </script>
 
-<style>
-.container {
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
+<style lang="sass">
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
+
